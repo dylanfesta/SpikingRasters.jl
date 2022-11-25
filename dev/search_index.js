05757f5bb647636c7d3af223b7339a7e39242582
Documenter.jl <documenter@juliadocs.github.io>
2022-11-25 13:06:03 +0000

    build based on 80d8529

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = SpikingRasters","category":"page"},{"location":"#SpikingRasters","page":"Home","title":"SpikingRasters","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for SpikingRasters.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [SpikingRasters]","category":"page"}]
+}
